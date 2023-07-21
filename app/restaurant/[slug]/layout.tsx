a9a534a158bad90f27769ec2b1c95e3bd27065c5
dalejo96 <dalejo_1996@hotmail.com>
2023-07-20 23:21:43 -0500

    Restaurant components fetching data

--- app/restaurant/[slug]/layout.tsx
+++ app/restaurant/[slug]/layout.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Header from "./components/Header";
 
-export default function RestaurantLayout({ children }: { children: React.ReactNode }) {
+interface Props {
+  children: React.ReactNode;
+  params: ParamsType;
+}
+
+interface ParamsType {
+  slug: string;
+}
+
+export default function RestaurantLayout({ children, params }: Props) {
   return (
     <main>
-      <Header />
+      <Header slug={params.slug} />
       <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">{children}</div>
     </main>
   );
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,22 +1,60 @@
-import NavBar from "../../components/NavBar";
 import Description from "./components/Description";
-import Header from "./components/Header";
 import Images from "./components/Images";
 import Rating from "./components/Rating";
 import ReservationCard from "./components/ReservationCard";
 import RestaurantNavBar from "./components/RestaurantNavBar";
 import Reviews from "./components/Reviews";
 import Title from "./components/Title";
+import { PrismaClient } from "@prisma/client";
 
-export default function RestaurantDetails() {
+const prisma = new PrismaClient();
+
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+
+  if (!restaurant) throw new Error('Restaurant not found')
+
+  return restaurant;
+};
+
+interface Props {
+  params: ParamProps;
+}
+
+interface ParamProps {
+  slug: string;
+}
+
+export default async function RestaurantDetails({ params }: Props) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
-        <RestaurantNavBar />
-        <Title />
+        <RestaurantNavBar slug={restaurant.slug} />
+        <Title name={restaurant.name} />
         <Rating />
-        <Description />
-        <Images />
+        <Description description={restaurant.description} />
+        <Images images={restaurant.images} />
         <Reviews />
       </div>
       <div className="w-[27%] relative text-reg">
