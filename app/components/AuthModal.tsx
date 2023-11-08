16b74162df645e56568931e14f412cafad78f446
dalejo96 <dalejo_1996@hotmail.com>
2023-11-07 20:36:55 -0500

    install MUI for modal and create two modals

---
+++ app/components/AuthModal.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import AuthModalInputs from "./AuthModalInputs";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
+
+interface AuthModalProps {
+  isSignin: boolean;
+}
+
+export default function AuthModal({ isSignin }: AuthModalProps) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <button className={`border p-1 px-4 rounded mr-3 ${isSignin && "bg-blue-400 text-white"}`} onClick={handleOpen}>
+        {isSignin ? "Sign in" : "Sign up"}
+      </button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div className="p-2">
+            <div className="uppercase font-bold text-center pb-2 border-b mb-2">
+              <p className="text-sm">{isSignin ? "Sign in" : "Create account"}</p>
+            </div>
+            <div className="m-auto" />
+            <h2 className="text-2xl font-light text-center">
+              {isSignin ? "Log Into Your Account" : "Create your OpenTable account"}
+            </h2>
+            <AuthModalInputs />
+            <button className="uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400">
+              {isSignin ? "Sign in" : "Create account"}
+            </button>
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
