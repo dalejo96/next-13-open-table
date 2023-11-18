27eb0320eb35148de7073eb5483f2986df9d90d9
dalejo96 <dalejo_1996@hotmail.com>
2023-11-18 09:21:49 -0500

    signin endpoint

--- pages/api/auth/signin.ts
+++ pages/api/auth/signin.ts
@@ -8,34 +8,18 @@ const prisma = new PrismaClient();
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    const { firstName, lastName, email, phone, city, password } = req.body;
+    const { email, password } = req.body;
 
     const errors: string[] = [];
 
     const validationSchema = [
       {
-        valid: validator.isLength(firstName, { min: 1, max: 20 }),
-        errorMessage: "First name is invalid",
-      },
-      {
-        valid: validator.isLength(lastName, { min: 1, max: 20 }),
-        errorMessage: "Last name is invalid",
-      },
-      {
         valid: validator.isEmail(email),
         errorMessage: "Email is invalid",
       },
       {
-        valid: validator.isMobilePhone(phone),
-        errorMessage: "Phone is invalid",
-      },
-      {
-        valid: validator.isLength(city, { min: 1, max: 20 }),
-        errorMessage: "City is invalid",
-      },
-      {
-        valid: validator.isStrongPassword(password),
-        errorMessage: "Password is not strong enough",
+        valid: validator.isLength(password, { min: 1 }),
+        errorMessage: "Password is invalid",
       },
     ];
 
@@ -53,26 +37,19 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       where: { email },
     });
 
-    if (userWithEmail) {
-      return res.status(400).json({ errorMessage: "Email is associated with another account. Please sign in " });
+    if (!userWithEmail) {
+      return res.status(401).json({ errorMessage: "Email or password is invalid" });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const isMatch = await bcrypt.compare(password, userWithEmail.password);
 
-    const user = await prisma.user.create({
-      data: {
-        first_name: firstName,
-        last_name: lastName,
-        password: hashedPassword,
-        city,
-        phone,
-        email,
-      },
-    });
+    if(!isMatch){
+      return res.status(401).json({ errorMessage: "Email or password is invalid" });
+    }
 
     const alg = "HS256";
     const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-    const token = await new jose.SignJWT({ email: user.email })
+    const token = await new jose.SignJWT({ email: userWithEmail.email })
       .setProtectedHeader({ alg })
       .setExpirationTime("24h")
       .sign(secret);
